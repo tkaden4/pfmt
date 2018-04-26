d6f50c27feb4b9e5a4f339e11da33c5df41f3e02
tkaden4 <thomas.kaden4@outlook.com>
2018-04-25 22:29:32 -0700

    Fix examples

---
+++ examples/position.c
@@ -0,0 +1,12 @@
+#include<pfmt/pfmt.h>
+
+#include<stdio.h>
+
+int main()
+{
+    pfmt_pos_t pos = { .x = 10, .y = 20 };
+    pfmt_set_position(&pos, pfmt_stdout_builder);
+    puts("Im at x = 10, y = 20!");
+    pfmt_reset(pfmt_stdout_builder);
+    return 0;
+}
